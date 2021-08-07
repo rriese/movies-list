e89b7065d5fdd70f17d95d2c467acb0a4a306cd5
rriese <rafael.riese@catolicasc.org.br>
2021-08-07 14:10:52 -0300

    Added process env key

--- src/api/MoviesService.js
+++ src/api/MoviesService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = process.env.REACT_APP_THE_MOVIE_DB_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const withBaseUrl = (path) => `${BASE_URL}${path}?api_key=${API_KEY}`
 
